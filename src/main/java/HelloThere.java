af976eebf1fda582a3b12bceb9a82e39876b5ec2
ysasayi <ysasayi@student.wethinkcode.co.za>
2021-05-11 21:27:22 +0200

    first commit

---
+++ src/main/java/HelloThere.java
@@ -0,0 +1,6 @@
+public class HelloThere {
+    public static void main(String[] args) {
+        System.out.println("hello there,");
+        System.out.println("chiller's project for git");
+    }
+}
